7d90fe67ce09681adae351def65c734843ea1975
jake <palatable7@naver.com>
2023-11-22 19:57:01 +0900

    Add textView

--- ExXcode/ViewController.swift
+++ ExXcode/ViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let textView = {
+        let v = UITextView()
+        v.text = "sample"
+        v.textColor = .black
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(textView)
+        NSLayoutConstraint.activate([
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            textView.topAnchor.constraint(equalTo: view.topAnchor),
+        ])
     }
 }
